887ad30326373a539fd16f7340c70454adbe2a46
StrangerAleks <Stranger_aleks@mail.ru>
2019-03-10 15:38:55 +0300

    add worked lab_02 Calculator.

---
+++ LAB_02_Calc/main.js
@@ -0,0 +1,26 @@
+function bCalculate() {
+  let value1=parseFloat(document.querySelector(".value1").value);
+  let value2=parseFloat(document.querySelector(".value2").value);
+  let operationType=document.querySelector(".operationType").value;
+  let result=0;
+  if ((value1 !=NaN) && (value2 !=NaN)) {
+    switch(operationType) {
+      case "+":
+        result = (value1 + value2).toFixed(3);
+        break;
+      case "-":
+        result = (value1 - value2).toFixed(3);
+        break;
+      case "*":
+        result = (value1 * value2).toFixed(3);
+        break;
+      case "/":
+        if (value2 != 0)
+          result = (value1 / value2).toFixed(3);
+        else
+          result = "Not allow zero dizion!"
+        break;
+    }
+    document.querySelector(".result").innerHTML=result;
+  }
+}
