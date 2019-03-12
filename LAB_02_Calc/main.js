7bd41a6996b2b9da53784cb70deaeb4db478073b
StrangerAleks <Stranger_aleks@mail.ru>
2019-03-12 12:37:28 +0300

    add LAB3 ready: EmptyBodyJSonlyCalculator

--- LAB_02_Calc/main.js
+++ LAB_02_Calc/main.js
@@ -3,7 +3,7 @@ function bCalculate() {
   let value2=parseFloat(document.querySelector(".value2").value);
   let operationType=document.querySelector(".operationType").value;
   let result=0;
-  if ((value1 !=NaN) && (value2 !=NaN)) {
+  if (value1 && value2) {
     switch(operationType) {
       case "+":
         result = (value1 + value2).toFixed(3);
