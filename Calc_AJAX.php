8fde222518e03f3ea4d3a9c3d0e4e29c94037a97
Dmitry <dmitry.cherepanov@citronium.com>
2019-03-27 16:43:34 +0300

    ajax

---
+++ Calc_AJAX.php
@@ -0,0 +1,34 @@
+<?php
+ini_set("display_errors", 1); // 1
+error_reporting(E_ALL);// 2
+
+function calc($a,$b,$op){
+
+    if($op=="+"){ $result=$a+$b;
+    }
+
+    if($op=="-"){ $result=$a-$b;
+    }
+
+    if($op=="*"){ $result=$a*$b;
+    }
+
+    if($op=="/"){
+	    if($b=="0"){
+        $result = "На нуль делить нельзя!";	
+	    }
+
+	    else { $result=$a/$b;	
+	    }
+    }
+    return $result;
+    }
+
+
+if (!empty($_REQUEST["calculate_button"])) {
+    $result = calc($_REQUEST["Input_A"],$_REQUEST["Input_B"],$_REQUEST["Operation"]); // 4 // 6.5
+    echo $result;
+    }
+
+
+
