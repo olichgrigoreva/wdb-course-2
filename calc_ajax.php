b4554bc7057a229de43cdf883edd778098234f32
AinazZ <48238731+AinazZ@users.noreply.github.com>
2019-04-03 11:56:48 +0300

    Add files via upload

--- calc_ajax.php
+++ calc_ajax.php
@@ -3,20 +3,19 @@
   $number1 = $_POST['number1'];
   $action = $_POST['action'];
   $number2 = $_POST['number2'];
-  if ($_POST['calculate']){
-   if (action=="+") {
-    $result = $number1 + $number2;
-   }
-   if (action=="-") {
-    $result = $number1 - $number2;
-   }
-   if (action=="*") {
-    $result = $number1 * $number2;
-   }
-   if (action=="/"){
-    $result = $number1 / $number2;
-   }
-  $result = 'Результат: ' + result;
+
+  if ($action=="+") {
+   $result = $number1 + $number2;
   }
+  elseif ($action=="-") {
+   $result = $number1 - $number2;
+  }
+  elseif ($action=="*") {
+   $result = $number1 * $number2;
+  }
+  elseif ($action=="/"){
+   $result = $number1 / $number2;
+  }
+  return $result;
  }
 ?>
