c22cd77e0138b3f2de45d6ffc70b8593469d515b
chirkov <rastajke@mail.ru>
2019-04-05 20:34:02 +0300

    ..

--- PHP_AJAX/php_calc.php
+++ PHP_AJAX/php_calc.php
@@ -3,27 +3,33 @@ ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
 function calculator($first_digit, $operation, $second_digit){
-	if($operation == "+"){
-	$resulstat = $first_digit+$second_digit;
+		
+	if($operation=="+"){
+	$resulstat=$first_digit+$second_digit;
 	}
-	if($operation == "-"){
-	$resulstat = $first_digit-$second_digit;
+	if($operation=="-"){
+	$resulstat=$first_digit-$second_digit;
 	}
-	if($operation == "*"){
-	$resulstat = $first_digit*$second_digit;
+	if($operation=="*"){
+	$resulstat=$first_digit*$second_digit;
 	}
-	if($operation == "/"){
-		if($second_digit == 0){
-			$resulstat = "На ноль делить нельзя";
+	if($operation=="/"){
+		if($second_digit == "0"){
+			$resulstat="На ноль делить нельзя";
 		}
-		else{$resulstat = $first_digit/$second_digit;
+		else{$resulstat=$first_digit/$second_digit;
 		}
 	}	
 	return $resulstat;
 }
 
-if (!empty($_REQUEST["count_up"])) {
-    $result = calculator($_REQUEST["first_field"],$_REQUEST["operation"],$_REQUEST["second_field"]);
-    }
-echo $result
+if ($_REQUEST["first_field"]!=="" && $_REQUEST["operation"]!=="" && $_REQUEST["second_field"]!==""){
+   		
+	$resulstat=calculator($_REQUEST["first_field"],$_REQUEST["operation"],$_REQUEST["second_field"]);
+    echo $resulstat;
+}
+
+else {
+	echo "Заполните все поля";
+}
 ?>
