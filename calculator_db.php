456c94404e25046c2421e9c41d4b283b851a58cb
AinazZ <48238731+AinazZ@users.noreply.github.com>
2019-04-07 17:47:39 +0300

    Add files via upload

---
+++ calculator_db.php
@@ -0,0 +1,58 @@
+<?php
+ ini_set("display_errors", 1);
+ error_reporting(E_ALL);
+ 
+ $connection = mysqli_connect("localhost", "root", "virtual", "wdb");
+
+ if (!empty ($connection)){
+  $query = mysqli_query ($connection, "SELECT * FROM results");
+ }
+
+ function calculate(){
+  $number1 = $_POST['number1'];
+  $action = $_POST['action'];
+  $number2 = $_POST['number2'];
+
+   if ($action=="+") {
+    $result = $number1 + $number2;
+   }
+   elseif ($action=="-") {
+    $result = $number1 - $number2;
+   }
+   elseif ($action=="*") {
+    $result = $number1 * $number2;
+   }
+   elseif ($action=="/") {
+    $result = $number1 / $number2;
+   }
+  return $result;
+ }
+
+ if (!empty($_REQUEST["submit"])) {
+  $result = calculate ($_REQUEST['number1'], $_REQUEST['action'], $_REQUEST['number2']);
+  $insert_query = mysqli_query ($connection, "INSERT INTO results(number1, action, number2, result) VALUES ('$number1', '$action', '$number2', '$result')");
+ }
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Калькулятор</title>
+ </head>
+ <body>
+  <form method="POST">
+   <input type="number" name="number1">
+   <select name="action">
+    <option>+</option>
+    <option>-</option>
+    <option>*</option>
+    <option>/</option>
+   </select>
+   <input type="number" name="number2">
+   <input type="submit" name="submit" value="calculate">
+  </form>
+  <?php echo $result;?>
+ <body>
+</html>
