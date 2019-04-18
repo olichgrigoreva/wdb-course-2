64be0a61432620d029ac8b071a153abe0a9c0377
Olga Grigoreva <aboutrocky94@gmail.com>
2019-04-19 02:02:20 +0300

    18.04.2019

--- auth_page/registration_data.php
+++ auth_page/registration_data.php
@@ -1,5 +1,7 @@
 <?php
 //$connection = mysqli_connect("localhost", "user", "user", "registration_data");//host,username,password,dbname,port,socket
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
 require("class_DB.php");
 $db = new Database("localhost", "user", "user", "grigorieva");
 
