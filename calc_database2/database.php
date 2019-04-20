210d13be896453043cb56b8ef76379dceab4f6d2
AinazZ <48238731+AinazZ@users.noreply.github.com>
2019-04-20 21:27:34 +0300

    Add files via upload

--- calc_database2/database.php
+++ calc_database2/database.php
@@ -6,16 +6,16 @@
      self::$connection = mysqli_connect("localhost", "root", "virtual", "wdb");
    }
 
-   public function query (){
+   public static function query (){
      self::$query = mysqli_query ($connection, "SELECT number1, action, number2, result FROM results ORDER BY created DESC LIMIT 5");
      while ($operation = mysqli_fetch_assoc ($query)){
       echo "<pre>";
       print_r ($operation);
       echo "</pre>";
-   }
+    }
+  }
 
-   function insert_query (){
+   public static function insert_query (){
      self::$insert_query = mysqli_query ($connection, "INSERT INTO results(number1, action, number2, result) VALUES ('$number1', '$action', '$number2', '$result')");
    }
- }
 ?>
