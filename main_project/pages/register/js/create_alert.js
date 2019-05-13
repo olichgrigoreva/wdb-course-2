f17d0151693532d225bfcda2de43046b285b818d
Ilya <pthev6@gmail.com>
2019-05-13 14:50:19 +0300

    Minor beauty fixes

--- main_project/pages/register/js/create_alert.js
+++ main_project/pages/register/js/create_alert.js
@@ -3,6 +3,7 @@
     function createAlert(divName, textNode) {
         divName.className = "alert alert-warning alert-dismissible mt-3"; // fade show почему то, работает некорректно, элемент остаётся невидимым после закрытия и повторного размещения
         divName.append(document.createTextNode(textNode));
+
         let closeButton = document.createElement("button");
             closeButton.type = "button";
             closeButton.classList.add("close");
@@ -13,6 +14,7 @@
             // cross.append(document.createTextNode("&times;"));
             // closeButton.append(cross);
             closeButton.innerHTML = "&times;";
+
         divName.append(closeButton);
     }
 }(app));
--- main_project/pages/register/js/create_element.js
+++ main_project/pages/register/js/create_element.js
@@ -2,10 +2,10 @@
 (function (name){
     name.createElmnt = createElmnt;
     function createElmnt(elementName, tagName, type, placeholder, className, parentDiv) {
-        elementName.type = type;
-        elementName.name = tagName;
+        elementName.type =        type;
+        elementName.name =        tagName;
         elementName.placeholder = placeholder;
-        elementName.className = className;
+        elementName.className =   className;
         parentDiv.append(elementName);
     }
 }(app));
