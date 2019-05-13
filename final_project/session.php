c549280bb4edee558b26c00d5fa0c1ec2cd947bc
AinazZ <48238731+AinazZ@users.noreply.github.com>
2019-05-13 12:16:46 +0300

    Delete session.php

--- final_project/session.php
+++
@@ -1,9 +0,0 @@
-<?php
- session_start();
-
- class Session {
-   public static function save_session(){
-     return self::$_SESSION["username"] = $_POST["username"];
-     return self::$_SESSION["password"] = $_POST["password"];
-   }
- }
