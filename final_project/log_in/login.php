b0dc7993dd5bca7f7ddf779bef1b54e5ae8a96d1
AinazZ <48238731+AinazZ@users.noreply.github.com>
2019-05-13 16:15:08 +0300

    Add files via upload

--- final_project/log_in/login.php
+++ final_project/log_in/login.php
@@ -44,7 +44,7 @@
 </header>
 <form method="post">
   <input type="text" name="username" id="username" placeholder="Username" />
-  <input type="text" name="password" id="password" placeholder="Password" />
+  <input type="password" name="password" id="password" placeholder="Password" />
   <input type="submit" name="submit" id="submit" value="Login" />
   <input type="button" name="button" id="button" value="Register" onclick="location.href = '../register/index.html'"/>
 </form>
