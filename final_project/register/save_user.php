e993a4b0d0229d942d67013321245a6bbacf64d2
AinazZ <48238731+AinazZ@users.noreply.github.com>
2019-05-27 12:44:05 +0300

    Add files via upload

--- final_project/register/save_user.php
+++ final_project/register/save_user.php
@@ -20,18 +20,31 @@
    if (!empty($username) && !empty($password) && !empty($confirm_pass) && !empty($email) && Validator::passwords_match($password, $confirm_pass)){
      $email = Validator::check_email($email);
 
-     Database::connect();
-     $sql_command = "SELECT * FROM `users` WHERE `username` = '$username'";
-     $query = "";
-     Database::query($sql_command);
-     $user  = "";
-     Database::fetch_assoc($query);
-
+     $connection = Database::connect();
+     echo "<pre>";
+     print_r ($connection);
+     echo "</pre>";
+     echo "<pre>";
+     print_r (mysqli_connect_error());
+     echo "</pre>";
+     echo "<pre>";
+     echo mysqli_errno($connection);
+     echo "</pre>";
+     echo "<pre>";
+     print_r (mysqli_error());
+     echo "</pre>";
+     /*$sql_command = "SELECT * FROM users_ziazova WHERE username='".$username."' ORDER BY username";
+     echo $sql_command;
+     $query = Database::query($sql_command);
      echo "<pre>";
      print_r ($query);
      echo "</pre>";
+     $user  = Database::fetch_assoc($query);
+     echo "<pre>";
+     print_r ($user);
+     echo "</pre>";
 
-     /*if (empty($user)){
+     if (empty($user)){
        $sql_command = "INSERT into users (username, password, email) VALUES ('$username', md5 '$password', '$email')";
        Database::query($sql_command);
 
