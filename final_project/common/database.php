a9e71463a91d81a0e30fe8caf02826c25da8388b
AinazZ <48238731+AinazZ@users.noreply.github.com>
2019-05-27 12:44:35 +0300

    Add files via upload

--- final_project/common/database.php
+++ final_project/common/database.php
@@ -1,17 +1,17 @@
 <?php
  class Database {
-   private static $connection;
-   private static $host   = 'localhost';
-   private static $user   = 'root';
-   private static $pass   = 'virtual';
-   private static $dbname = 'ziazova';
+   public    static $connection;
+   private   static $host   = "localhost";
+   private   static $user   = "root";
+   private   static $pass   = "virtual";
+   private   static $dbname = "wdb";
 
    public static function connect(){
      return self::$connection = mysqli_connect(self::$host, self::$user, self::$pass, self::$dbname) or die ("Невозможно соединиться с базой данных");
    }
 
    public static function query($sql_command){
-     return $query = mysqli_query(self::$connection, $sql_command) or die ("Неправильный запрос!");
+     return $sql_query = mysqli_query(self::$connection, $sql_command) or die ("Неправильный запрос!");
    }
 
    public static function fetch_assoc($query){
