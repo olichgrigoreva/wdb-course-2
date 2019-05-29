a07fbfde0257c9f7ac3edf708aec38509bd6b2b1
AinazZ <48238731+AinazZ@users.noreply.github.com>
2019-05-29 13:59:21 +0300

    Delete index.js

--- final_project/notebook/profile/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import ProfileForm from "./elements/form.js";
-
-document.addEventListener("DOMContentLoaded", function (){
-  ReactDOM.render(<ProfileForm />, document.querySelector(".root"));
-})
