a481462c49fd451a21fb99bfe4ee072a7e213300
Delrm <ipdelirium@gmail.com>
2019-06-19 23:11:29 +0300

    build

---
+++ Notebook/note-r/build/precache-manifest.74d1932146f45d9548b618a89e3a9b95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b3038ae0d96188fea8d0482fd814b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "148291e2c5f30f6e9d79",
+    "url": "/static/css/main.8bfb6839.chunk.css"
+  },
+  {
+    "revision": "ef95c6da83778b21e97a",
+    "url": "/static/js/2.6144fa04.chunk.js"
+  },
+  {
+    "revision": "148291e2c5f30f6e9d79",
+    "url": "/static/js/main.b5c8240d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
