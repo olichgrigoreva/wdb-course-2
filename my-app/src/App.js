d534b7d50b27f32165ac0d0515bcb1845b3861a0
Korotkii <korotki27@gmail.com>
2019-07-07 11:08:03 +0300

    reactJS

---
+++ my-app/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="my-flex-container">
+      <div className="my-flex-block">
+      </div>
+      <div className="my-flex-block">
+      </div>
+      <div className="my-flex-block">
+      </div>
+      <div className="my-flex-block">
+      </div>
+      <div className="my-flex-block">
+      </div>
+      <div className="my-flex-block">
+      </div>
+      <div className="my-flex-block">
+      </div>
+      <div className="my-flex-block">
+      </div>
+      <div className="my-flex-block">
+      </div>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
